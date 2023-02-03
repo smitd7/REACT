d46e47a69875196963dcf7c9cb3e629e34a7c719
smitd7 <117810432+smitd7@users.noreply.github.com>
2023-02-03 13:53:15 +0530

    added expense items

---
+++ ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return (
+    <div>
+        <h2>Expense Items</h2>
+        <div>food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+    </div>)
+}
+export default ExpenseItem;
