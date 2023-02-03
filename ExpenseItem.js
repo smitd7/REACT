4015b63744737c0b036cd1b5da9e20107706a142
smitd7 <117810432+smitd7@users.noreply.github.com>
2023-02-03 18:32:53 +0530

    location added

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -7,6 +7,7 @@ function ExpenseItem(props){
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div> 
+            <div className='epense-item-location'>{props.location}</div>
         </div>
     </div>)
 }
