9d79357d7a74fd39863de517c4f7dc22faa4065e
smitd7 <117810432+smitd7@users.noreply.github.com>
2023-02-03 18:26:37 +0530

    use of props

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,10 +1,14 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
+
+function ExpenseItem(props){
     return (
-    <div>
-        <h2>Expense Items</h2>
-        <div>food Rs 10</div>
-        <div>Petrol Rs 100</div>
-        <div>Movies Rs 200</div>
+    <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div> 
+        </div>
     </div>)
 }
+
 export default ExpenseItem;
