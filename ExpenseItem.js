d9761fe7794e71af508ecf445d7bda2f4bc880ea
smitd7 <117810432+smitd7@users.noreply.github.com>
2023-02-07 12:51:38 +0530

    delete button added

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,22 +1,28 @@
-import ExpenseDate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails";
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-  
-  return (
-    <div className="expense-item">
-        <ExpenseDate date={props.date}></ExpenseDate>
-        <ExpenseDetails title={props.title}></ExpenseDetails>
-        <ExpenseDetails amount={props.amount}></ExpenseDetails>
-      
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <div className="epense-item-location">{props.location}</div>
-      </div>
-    </div>
-  );
+function ExpenseItem(props){
+    const clickHandler=()=>{
+        console.log('clicked!!!');
+    };
+    const deleteExpense=()=>{
+        console.log('Expense deleted!!!');
+    }
+    
+    // const expenseDate=new Date(2023,2,28)
+    // const expenseTitle='car insurance'
+    // const expenseAmount=500000
+    return(
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={deleteExpense}>Delete Expense</button>
+        </Card>
+    )
 }
-
 export default ExpenseItem;
